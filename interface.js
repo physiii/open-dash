066baa3d3a451f957fec461d90e967764bead2ef
Jeremy <jeremy-pollock@raider.rose.edu>
2018-01-10 17:37:23 -0600

    Added remote.js & moved remote functions into new module

--- interface.js
+++ interface.js
@@ -7,14 +7,12 @@ var win = gui.Window.get();
 var child_process = require('child_process')
 var exec = child_process.exec;
 var spawn = child_process.spawn;
-var child = child_process.spawn(process.execPath, [], {detached: true});
 var fs = require('fs');
 var ping = require('ping');
 var utils = require('./utils.js');
 var database = require('./database.js');
 var update = require('./update.js');
-
-
+var remote = require('./remote');
 var nwpm_socket = require('socket.io-client')("http://127.0.0.1:1235");
 
 
@@ -102,58 +100,8 @@ window.onresize = function() {
   updateContentStyle();
 };
 
-// Functions to Launch and Close VNC Client
-var vnc_ip = "192.168.0.16";
 
-var vnc_client;
-vnc_started = false;
-function start_vnc() {
-  if (vnc_started) return;
-  vnc_started = true;
-  vnc_client = spawn('vinagre', ['-f', '192.168.0.16::5900']);
-  vnc_client.stdout.on('data', function (data) {
-    console.log('stdout: ' + data);
-  });
-
-  vnc_client.stderr.on('data', function (data) {
-    console.log('stderr: ' + data);
-  });
-
-  vnc_client.on('close', function (code) {
-    console.log('child process exited with code ' + code);
-  });
-}
-
-function close_vnc() {
-  if (!vnc_started) return;
-  vnc_client.kill();
-  vnc_started = false;
-
-}
-
-// Timeout with connection testing
-timeout();
-function timeout() {
-  setTimeout(function () {
-    console.log("checking mdd connection...")
-    check_mdd_conn();
-    timeout();
-  }, 1*1000);
-}
-
-function check_mdd_conn() {
-    ping.sys.probe(vnc_ip, function(isAlive){
-        var msg = isAlive ? 'host ' + vnc_ip + ' is alive' : 'host ' + vnc_ip + ' is dead';
-        if (isAlive) {
-	  start_vnc();
-        }
-        if (!isAlive) {
-	  close_vnc();
-        }
-    });
-}
-
-// Main application running moduiles
+// Main application running modules
 const express = require('express')
 const app = express()
 
@@ -162,7 +110,7 @@ app.get('/mdd', function(req, res) {
 });
 
 app.get('/switch-to-vnc', function(req, res) {
-  start_vnc();
+  remote.start_vnc();
   res.send('Hello MDD!');
 })
 app.listen(3000, function() {console.log('Example app listening on port 3000!')})
@@ -173,38 +121,30 @@ console.log("HELLO!!!!!!");
 window.onload = function() {
 
   document.getElementById("vnc_dashboard_btn").onclick = function() {
-    start_vnc();
+    remote.start_vnc();
     win.minimize();
   };
   
   document.getElementById("phone_btn").onclick = function() {
-    //win.close();
+    //Socket to nwpm to reboot function
     nwpm_socket.emit('reboot', true)
   
   };
 
   document.getElementById("radio_btn").onclick = function() {
-    //win.close();
+    //Socket to nwpm to cancel reboot function
     nwpm_socket.emit('canc_reboot', true)
   
   };
 
   document.getElementById("settings_btn").onclick = function() {
-    //update.pull()
-
-    //Hide currently window session prior to reload of session with 5 sec delay.
-    
+    //Socket to nwpm to update function
     nwpm_socket.emit('update',true);
-    //console.warn("We are reloading the window in 5 seconds.")
-    //setTimeout(function() {win.hide();}, 5000);
-    //setTimeout(function() {win.reload();}, 5000);
-
 
   };
 
   updateContentStyle();
   win.show();
-
 
 };
 
---
+++ remote.js
@@ -0,0 +1,72 @@
+// ----------------------------  open-dash --- ----------------------------- //
+// ---------------  https://github.com/physiii/open-dash  ------------------ //
+// ----------------------------- remote.js ---------------------------------- //
+
+var child_process = require('child_process')
+var exec = child_process.exec;
+var spawn = child_process.spawn;
+var fs = require('fs');
+var ping = require('ping');
+var utils = require('./utils.js');
+var database = require('./database.js');
+var update = require('./update.js');
+var remote_socket= require('socket.io-client')("http://127.0.0.1:1236");
+
+module.exports = {
+  start_vnc: start_vnc,
+  close_vnc: close_vnc,
+  timeout: timeout,
+  check_mdd_conn: check_mdd_conn,
+
+}
+
+
+var vnc_ip = "192.168.0.16";
+
+var vnc_client;
+vnc_started = false;
+function start_vnc() {
+  if (vnc_started) return;
+  vnc_started = true;
+  vnc_client = spawn('vinagre', ['-f', '192.168.0.16::5900']);
+  vnc_client.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+  });
+
+  vnc_client.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+  });
+
+  vnc_client.on('close', function (code) {
+    console.log('child process exited with code ' + code);
+  });
+}
+
+function close_vnc() {
+  if (!vnc_started) return;
+  vnc_client.kill();
+  vnc_started = false;
+
+}
+
+// Timeout with connection testing
+timeout();
+function timeout() {
+  setTimeout(function () {
+    console.log("checking mdd connection...")
+    check_mdd_conn();
+    timeout();
+  }, 1*1000);
+}
+
+function check_mdd_conn() {
+    ping.sys.probe(vnc_ip, function(isAlive){
+        var msg = isAlive ? 'host ' + vnc_ip + ' is alive' : 'host ' + vnc_ip + ' is dead';
+        if (isAlive) {
+	  start_vnc();
+        }
+        if (!isAlive) {
+	  close_vnc();
+        }
+    });
+}
