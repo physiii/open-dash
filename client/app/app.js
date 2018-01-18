a06e9a31394bf2b5621e5a0adaadb4db2d7254c0
meghana <megk0890@gmail.com>
2018-01-17 20:27:46 -0600

    Added routing

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,14 @@
 'use strict';
 
-angular.module('openDashApp', ['ngResource','ngRoute']);
+var app = angular.module('app', ['ngRoute']);
 
-
+app.config(function ($routeProvider) {
+    $routeProvider.
+    when('/', {
+        templateUrl: 'app/main/view/dashboard.html',
+        controller: 'DashboardController'
+    }).
+    otherwise({
+        redirectTo: '/'
+    });
+});
