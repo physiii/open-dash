6ea5bff7ce5e8d7229753061ddb80d4a345ed8ea
Jeremy <jeremy-pollock@raider.rose.edu>
2018-01-18 12:21:31 -0600

    Added btn click sound and function in audio.js

--- modules/audio.js
+++ modules/audio.js
@@ -2,24 +2,31 @@
 // -----------------  https://github.com/physiii/Open-Dash -------------------- //
 // ---------------------------------- audio.js ------------------------------------ //
 
-var blank = null;
+//var ffmpeg = require('ffmpeg');
+var Sound = require('aplay');
+var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+var fs = require('fs');
+var TAG = "[audio.js]";
+var ffmpeg_pass = [];
+var command = [];
+ffmpeg_timer = setTimeout(function () {}, 1);
 
 
 module.exports = {
+  btn_click: btn_click,
 
 };
 
-
-function blank() {
-  return;
-};
-
+function btn_click() {
+  new Sound().play('./mouse-click.wav');
+}
 
 //Socket.io functionality if needed
 
-
-
 //--End of code prior to test function for module--//
+
 function test() {
   console.log("Testing audio Module");
-};
+  btn_click()
+}
