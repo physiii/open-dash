c6952d175169daa93b54f23306370726a40e6f39
Jeremy <jeremy-pollock@raider.rose.edu>
2018-01-18 18:49:47 -0600

    modified promises to include functions instead of individual files.

--- modules/audio.js
+++ modules/audio.js
@@ -21,60 +21,33 @@ module.exports = {
 };
 
 function btn_click() {
-  new Promise(function(resolve, reject) {
-    var click = new Sound();
-    click.play('./sounds/mouse-click.wav');
+  var click = new Sound();
+  click.play('./sounds/mouse-click.wav');
+  return new Promise(function(resolve, reject) {
     click.on('complete', function() {
       resolve(true);
     });
   });
-  //new Sound().play('./sounds/mouse-click.wav');
 }
 
 function alert_snd() {
-  new Promise(function(resolve, reject) {
-    var alert = new Sound();
-    alert.play('./sounds/mouse-click.wav');
+  var alert = new Sound();
+  alert.play('./sounds/alert_snd.wav');
+  return new Promise(function(resolve, reject) {
     alert.on('complete', function() {
       resolve(true);
     });
   });
-  //new Sound().play('./sounds/alert_snd.wav');
 }
  
 
-function playSoundFile(filename) {
-  var music = new Sound();
-  music.play(filename);
-  return new Promise(function(resolve, reject) {
-    music.on('complete', function() {
-       resolve(true);
-    });
-  });
-}
 //Socket.io functionality if needed
 
 //--End of code prior to test function for module--//
 
-function testSoundsInSequence() {
-  console.log("Testing audio Module");
-  playSoundFile('./sounds/mouse-click.wav').then(function() {
-     playSoundFile('./sounds/alert_snd.wav');
-  });
-}
-
 function test() {
-  console.log("Testing audio Module");
-  btn_click();
-  setTimeout(function() {alert_snd();}, 750);
-}
-
-function test2() {
   console.log("Testing audio Module");
   btn_click().then(function(){
      alert_snd();
   })
-
 }
-
-test2()
