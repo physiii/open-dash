036ce04b4ba5eb606aeff8566957691db1741637
mahesh <nillauid201@gmail.com>
2018-01-18 20:13:25 -0600

    created header and footer templating, added into index.html

--- client/app/app.js
+++ client/app/app.js
@@ -11,3 +11,25 @@ app.config(function ($routeProvider) {
         redirectTo: '/'
     });
 });
+app.directive('dashboardHeader', function () {
+    return {
+        restrict: 'E', //This menas that it will be used as an element.
+        templateUrl: "app/main/js/directives/header.html",
+        controller: ['$scope', '$filter', function ($scope, $filter) {
+            // Your behaviour goes here :)
+
+            console.log("loading header !!!!");
+        }]
+    }                   
+});
+app.directive('dashboardFooter', function () {
+    return {
+        restrict: 'E', //This menas that it will be used as an element.
+        templateUrl: "app/main/js/directives/footer.html",
+        controller: ['$scope', '$filter', function ($scope, $filter) {
+            // Your behaviour goes here :)
+
+            console.log("loading footer !!!!");
+        }]
+    }                   
+});
