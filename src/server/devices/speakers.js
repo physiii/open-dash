e76228780802901d06318db16e8fa5612d6e1f69
Jeremy <jeremy-pollock@raider.rose.edu>
2018-01-21 12:04:20 -0600

    Updated

--- src/server/devices/speakers.js
+++ src/server/devices/speakers.js
@@ -1,5 +1,4 @@
 // ------------------------------  OPEN-DASH ----------------------------------- //
-// ------------------------------  OPEN-DASH ----------------------------------- //
 // -----------------  https://github.com/physiii/open-dash/devices  -------------------- //
 // ---------------------------------- speakers.js --------------------------------------- //
 
@@ -7,169 +6,70 @@
 
 //module for system (master) volume controls
 
-var module = require('module');
-var platform = require('platform')
+
 var spawn = require('child_process').spawn;
-var loudness = require('loudness');
 
 module.exports = {
-  getVolume: getVolume,
-  setVolume: setVolume,
   raiseVolume: raiseVolume,
   lowerVolume: lowerVolume,
-  setMuted: setMuted,
-  getMuted: getMuted,
   mute: mute,
   play: play,
   next: next,
   speaker_test: speaker_test,
 }
 
-// Volume: up, down, set, mute
-
-function getVolume() {
-  return promise=new Promise( function(resolve, reject) {
-    loudness.getVolume(function(err, vol) {
-      if(err) reject(err);
-      else resolve(vol);
-    });
-  });
-}
-
-//volume range is 0-100
-
-function setVolume(vol) {
-  return promise=new Promise( function(resolve, reject) {
-    loudness.setVolume(vol, function(err) {
-      if(err) reject(err);
-      else resolve(vol);
-    });
-  });
-}
-
-
-
 //raise volume by 5 units
 function raiseVolume() {
-  alert('volume raise');
-  return promise=new Promise( function(resolve, reject) {
-     getVolume().then(function(err, vol) {
-      if(err) return reject(err);
-      loudness.setVolume(vol+5, function(seterr) {
-          if(seterr) reject(seterr);
-          else resolve(vol);
-      });
-     });
+  return new Promise( function(resolve, reject) {
+    spawn('xdotool', ['key', 'XF86AudioRaiseVolume']);
+    resolve(true);
   });
-}
+};
+
 //lower volume by 5 units
 function lowerVolume() {
-  return promise=new Promise( function(resolve, reject) {
-    getVolume().then(function(err, vol) {
-      if(err) return reject(err);
-      loudness.setVolume(vol-5, function(seterr) {
-        if(seterr) reject(seterr);
-        else resolve(vol);
-      });
-    });
+  return new Promise( function(resolve, reject) {
+    spawn('xdotool', ['key', 'XF86AudioLowerVolume']);
+    resolve(true);
   });
-}
+};
 
-function getMuted() {
-  return promise=new Promise( function(resolve, reject) {
-    loudness.getMuted(function(err, mute) {
-      if(err) reject(err);
-      else resolve(mute);
-    });
-  });
-}
-function setMuted(mute) {
-  return promise=new Promise( function(resolve, reject) {
-    loudness.setMuted(mute, function(err) {
-      if(err) reject(err);
-      else resolve(mute);
-    });
-  });
-}
 
 //mute if not-muted, unmute if muted
 function mute() {
-  loudness.setVolume(45, function (err) { 
+  return new Promise( function(resolve, reject) {
+    spawn('xdotool', ['key', 'XF86AudioMute']);
+    resolve(true);
   });
-  return promise=new Promise( function(resolve, reject) {
-    getMuted().then(function(err, mute) {
-      if(err) return reject(err);
-      loudness.setMuted(!mute, function(seterr) {
-        if(seterr) reject(seterr);
-        else resolve(!mute);
-      });
-    });
-  });
-}
+};
 
 //functions play, pause, resume, next should be in media player
 //but leaving play and next here for now
 function play() {
-   spawn('xdotool', ['key', 'XF86AudioPlay']);
-}
+  return new Promise( function(resolve, reject) {
+    spawn('xdotool', ['key', 'XF86AudioPlay']);
+    resolve(true);
+  });
+};
+
 function next() {
-  spawn('xdotool', ['key', 'XF86AudioNext']);
-}
-
-
-//Windows Functions
-function win_command(data) {loudness.setVolume(45, function (err) {
-  // Done 
-});
-  var command = data.cmd;
-  console.log("running"+platform+"platform")
-  if (command == "volume down"){
-    spawn('nircmd.exe', ['mutesysvolume', '0']);
-    spawn('nircmd.exe', ['changesysvolume', '-5000']);
-  }
-  if (command == "volume up"){
-    spawn('nircmd.exe', ['mutesysvolume', '0']);
-    spawn('nircmd.exe', ['changesysvolume', '+5000']);
-  }
-  if (command == "mute"){ spawn('nircmd.exe', ['mutesysvolume', '1']) }
-  if (command == "play"){ spawn('nircmd.exe', ['mutesysvolume', '1']) }
-
-};
-
-//Linux Functions
-function linux_command(data) {
-  var command = data.cmd;
-  console.log("running"+platform+"platform")
-  if ( command === "volume down" ) { spawn('xdotool', ['key', 'XF86AudioLowerVolume']) }
-  if ( command === "volume up" ) { spawn('xdotool', ['key', 'XF86AudioRaiseVolume']) }
-  if ( command === "mute" ) { spawn('xdotool', ['key', 'XF86AudioMute']) }
-  if ( command === "play" ) { spawn('xdotool', ['key', 'XF86AudioPlay']) }
-  if ( command === "next" ) { spawn('xdotool', ['key', 'XF86AudioNext']) }
-  //for volume slider use: xodotool amixer -c 0 sset Master,0 80%
-
-};
-
-//Unsupported platform
-function unsupp_command(data) {
-  var command = data.cmd;
-  console.log("Unsupported platform"+ platform);
-  if ( command === "volume down" ) { spawn('xdotool', ['key', 'XF86AudioLowerVolume']) }
-  if ( command === "volume up" ) { spawn('xdotool', ['key', 'XF86AudioRaiseVolume']) }
-  if ( command === "mute" ) { spawn('xdotool', ['key', 'XF86AudioMute']) }
-  if ( command === "play" ) { spawn('xdotool', ['key', 'XF86AudioPlay']) }
-  if ( command === "next" ) { spawn('xdotool', ['key', 'XF86AudioNext']) }
-
+  return new Promise( function(resolve, reject) {
+    spawn('xdotool', ['key', 'XF86AudioNext']);
+    resolve(true);
+  });
 };
 
 //Test Functions
 
 function speaker_test() {
   console.log("Testing speaker Module");
-  getVolume().then(function(){
+  lowerVolume().then(function(){
     return raiseVolume();
-  }).then(function(vo) {
+  }).then(function() {
     return lowerVolume();
   }).then(function() {
-    console.log("Finished running test.") 
+    return mute();
+  }).then(function() {
+    console.log("Finished running test.")
   })
 };
