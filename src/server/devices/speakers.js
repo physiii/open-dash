72fca4232ead215ad1635e97abdfcead84fcf61f
Jeremy <jeremy-pollock@raider.rose.edu>
2018-01-22 08:33:10 -0600

    Readded previous promise codes for volume control

--- src/server/devices/speakers.js
+++ src/server/devices/speakers.js
@@ -74,3 +74,91 @@ function speaker_test() {
     console.log("Finished running test.")
   })
 };
+
+
+
+/*
+var loudness = require('loudness');
+
+function getVolume() {
+  return promise=new Promise( function(resolve, reject) {
+    loudness.getVolume(function(err, vol) {
+      if(err) reject(err);
+      else resolve(vol);
+    });
+  });
+}
+
+//volume range is 0-100
+
+function setVolume(vol) {
+  return promise=new Promise( function(resolve, reject) {
+    loudness.setVolume(vol, function(err) {
+      if(err) reject(err);
+      else resolve(vol);
+    });
+  });
+}
+
+
+
+//raise volume by 5 units
+function raiseVolume() {
+  return promise=new Promise( function(resolve, reject) {
+     getVolume().then(function(err, vol) {
+      if(err) return reject(err);
+      loudness.setVolume(vol+5, function(seterr) {
+          if(seterr) reject(seterr);
+          else resolve(vol);
+      });
+     });
+  });
+}
+//lower volume by 5 units
+function lowerVolume() {
+  return promise=new Promise( function(resolve, reject) {
+    getVolume().then(function(err, vol) {
+      if(err) return reject(err);
+      loudness.setVolume(vol-5, function(seterr) {
+        if(seterr) reject(seterr);
+        else resolve(vol);
+      });
+    });
+  });
+}
+
+function getMuted() {
+  return promise=new Promise( function(resolve, reject) {
+    loudness.getMuted(function(err, mute) {
+      if(err) reject(err);
+      else resolve(mute);
+    });
+  });
+}
+function setMuted(mute) {
+  return promise=new Promise( function(resolve, reject) {
+    loudness.setMuted(mute, function(err) {
+      if(err) reject(err);
+      else resolve(mute);
+    });
+  });
+}
+
+//mute if not-muted, unmute if muted
+function mute() {
+  return promise=new Promise( function(resolve, reject) {
+    getMuted().then(function(err, mute) {
+      if(err) return reject(err);
+      loudness.setMuted(!mute, function(seterr) {
+        if(seterr) reject(seterr);
+        else resolve(!mute);
+      });
+    });
+  });
+}
+
+*/
+
+
+
+
