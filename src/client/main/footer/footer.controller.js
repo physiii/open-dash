f6ea1383bdfad5b1651ca0780b615ef9496d8f85
Jeremy <jeremy-pollock@raider.rose.edu>
2018-01-22 10:04:08 -0600

    Updated

--- src/client/main/footer/footer.controller.js
+++ src/client/main/footer/footer.controller.js
@@ -1,9 +1,5 @@
 var app = angular.module('app');
-<<<<<<< HEAD
 var speakers = require('../server/devices/speakers.js');
-=======
-var speakers = require('../server/devices/speakers');
->>>>>>> 20448e2f441aba24119c5dce62d00bfa77d647e0
 
 app.controller('FooterController', function($scope,$location){
     $scope.showVolumeSlider = false;
