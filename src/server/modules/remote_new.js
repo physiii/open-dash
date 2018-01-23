bac5b717d3e8f8c4555116c59f54b50d9bca17d1
Jeremy <jeremy-pollock@raider.rose.edu>
2018-01-23 11:44:26 -0600

    added nmap functionality to new remote.

--- src/server/modules/remote_new.js
+++ src/server/modules/remote_new.js
@@ -7,17 +7,27 @@ var child_process = require('child_process');
 var exec = child_process.exec;
 var spawn = child_process.spawn;
 var database = require('../database.js');
-
+var ip = require("ip");
 
 var vnc_ip = "127.0.0.1";
 var vnc_port= "5900";
 var vnc_client;
 vnc_started = false;
+my_ip = ip.address();
 
 module.exports = {
   start_vnc: start_vnc,
   close_vnc: close_vnc,
 }
+
+function runScan(){
+  exec('sudo nmap -sn 192.168.'+my_ip[8]+'.1/24',function(err,stdout,stderr){
+    if (err){
+      console.error('exec error: ' + err);
+    }
+    console.log(stdout);
+  });
+};
 
 function start_vnc() {
   console.log("Starting vnc services. . .")
@@ -32,8 +42,11 @@ function close_vnc() {
 }
 
 
-function remoteTest();{
-
-}
-
-start_vnc();
+function remoteTest(){
+  console.log('Running host discovery scan. Please wait. . .');
+  runScan().then(function(){
+    return runScan2();
+  }).then(function(){
+    console.log(result);
+  });
+};
