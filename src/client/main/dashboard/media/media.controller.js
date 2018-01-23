3528b10f91ecf35be35cd35e33d6c7f205a1a2f4
Naga Sadam <nagasadam7@gmail.com>
2018-01-23 11:49:46 -0500

    loads play list from media folder, and lets you skip previous or skip next

--- src/client/main/dashboard/media/media.controller.js
+++ src/client/main/dashboard/media/media.controller.js
@@ -1,6 +1,14 @@
 var app = angular.module('app');
+var player = require('../server/modules/player.js');
 
 app.controller('MediaController', function ($scope, $location) {
+
+  player.getAudioFiles("../../../media").then(function (files) {
+    $scope.audioFiles = files.map(function (file) {
+      return "../../media/" + file;
+    });
+    $scope.currentIndex = 0;
+  });
 
   $scope.back = function () {
     $location.path('dashboard');
@@ -9,6 +17,10 @@ app.controller('MediaController', function ($scope, $location) {
     var audio = document.getElementById("audioTrack");
     if (!$scope.audio) {
       $scope.audio = audio;
+      if (!audio.src) {
+        audio.src = $scope.audioFiles[0];
+        $scope.currentIndex = 0;
+      }
       $scope.audio.load();
     }
     var button = document.getElementById("playButton");
@@ -22,6 +34,24 @@ app.controller('MediaController', function ($scope, $location) {
     else {
       audio.pause();
       button.innerHTML = "play_arrow";
+    }
+  }
+  $scope.skipPrevious = function () {
+    if ($scope.currentIndex > 0) {
+      var audioPaused = $scope.audio.paused;
+      $scope.currentIndex -= 1;
+      $scope.audio.src = $scope.audioFiles[$scope.currentIndex];
+      $scope.audio.load();
+      if (!audioPaused) $scope.audio.play();
+    }
+  }
+  $scope.skipNext = function () {
+    if ($scope.currentIndex < ($scope.audioFiles.length - 1)) {
+      var audioPaused = $scope.audio.paused;
+      $scope.currentIndex += 1;
+      $scope.audio.src = $scope.audioFiles[$scope.currentIndex];
+      $scope.audio.load();
+      if (!audioPaused) $scope.audio.play();
     }
   }
 });
