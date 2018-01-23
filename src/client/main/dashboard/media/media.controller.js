151b038ca625bfea4189f1eac1d1d8cada5cec3c
Naga Sadam <nagasadam7@gmail.com>
2018-01-23 09:07:21 -0500

    Added play audio

--- src/client/main/dashboard/media/media.controller.js
+++ src/client/main/dashboard/media/media.controller.js
@@ -1,7 +1,27 @@
 var app = angular.module('app');
 
-app.controller('MediaController', function($scope,$location){
-    $scope.back=function(){
-        $location.path('dashboard');
+app.controller('MediaController', function ($scope, $location) {
+
+  $scope.back = function () {
+    $location.path('dashboard');
+  }
+  $scope.playMedia = function () {
+    var audio = document.getElementById("audioTrack");
+    if (!$scope.audio) {
+      $scope.audio = audio;
+      $scope.audio.load();
     }
+    var button = document.getElementById("playButton");
+    audio.addEventListener('loadedmetadata', function (metadata) {
+      console.log("duration=", audio.duration);
+    });
+    if (audio.paused) {
+      audio.play();
+      button.innerHTML = "pause";
+    }
+    else {
+      audio.pause();
+      button.innerHTML = "play_arrow";
+    }
+  }
 });
