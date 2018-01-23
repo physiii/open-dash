47a13e68c23f5de3a9fe6c4a8f3fe95858d756bd
meghana <megk0890@gmail.com>
2018-01-23 17:39:34 -0500

    Added back button in the header,Added sample json for remote.js

--- src/client/main/dashboard/remote/remote.controller.js
+++ src/client/main/dashboard/remote/remote.controller.js
@@ -4,4 +4,28 @@ app.controller('RemoteController', function($scope,$location){
     $scope.back=function(){
         $location.path('/');
     }
+    $scope.remoteAddressInfo =[
+        {
+            local_IP:"10.10.10.10",
+            hostname:"hostname1"
+        },
+        {
+            local_IP:"10.10.10.11",
+            hostname:"hostname2"
+        },
+        {
+            local_IP:"10.10.10.10",
+            hostname:"hostname1"
+        },
+        {
+            local_IP:"10.10.10.11",
+            hostname:"hostname2"
+        },
+        {
+            local_IP:"10.10.10.10",
+            hostname:"hostname1"
+        },
+
+
+    ]
 });
