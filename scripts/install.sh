a3bf650ded7ef51a2551e2c9d68e8d8b3d4c419f
physiii <andy@pyfi.org>
2018-01-26 16:18:19 -0600

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@ curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt update
 
 sudo apt-get install -y \
-nodejs xinput npm libgconf2-4 gnome-tweak-tool net-tools chromium-browser nmap mongodb git openssh-server tmux
+ dnsmasq hostapd nodejs xinput npm libgconf2-4 gnome-tweak-tool net-tools chromium-browser nmap mongodb git openssh-server tmux
 
 sudo npm install -g pm2 nw  #--unsafe-perm=true --allow-root
 sudo python -m pip install pymongo numpy opencv-python
