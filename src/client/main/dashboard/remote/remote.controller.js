0d1744c3e85481c00846958fa230296fa3bdb2c9
meghana <megk0890@gmail.com>
2018-01-26 18:45:10 -0500

    Added Scan and autoconnect buttons

--- src/client/main/dashboard/remote/remote.controller.js
+++ src/client/main/dashboard/remote/remote.controller.js
@@ -1,6 +1,6 @@
 var app = angular.module('app');
 var remote = require('../server/modules/remote.js');
-app.controller('RemoteController', function($scope,$rootScope,$location){
+app.controller('RemoteController', function($scope,$rootScope,$location,$timeout){
     $scope.back=function(){
         $location.path('/');
     }
@@ -8,9 +8,18 @@ app.controller('RemoteController', function($scope,$rootScope,$location){
     $scope.connectRemote = function(ip){
         remote.connect(ip,null);
     }
+    $scope.runScan = function(ip){
+        remote.connect(ip,null);
+    }
     $scope.autoConnectLocalIp = function(details){
        if(details.toggleIp){
            remote.set_autoconnect_device(details.local_ip);
        }
     };
+    $scope.manualRun = function(){
+        remote.runScan().then(function (list) {
+            $rootScope.remoteAddressInfo = list;
+            $rootScope.$apply();
+        });
+    }
 });
