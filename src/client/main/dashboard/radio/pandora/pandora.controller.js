348ce35256d61197d93e43d531a215a1edd34134
meghana <megk0890@gmail.com>
2018-02-13 11:54:23 -0500

    Added view to pandora page Added Scroll Bar

--- src/client/main/dashboard/radio/pandora/pandora.controller.js
+++ src/client/main/dashboard/radio/pandora/pandora.controller.js
@@ -4,4 +4,11 @@ app.controller('PandoraController', function($scope,$location){
     $scope.back=function(){
         $location.path('radio');
     }
+
+    $scope.vm = {
+        formData: {
+            email: 'hello@patternry.com',
+            password: 'foobar'
+        }
+    };
 });
