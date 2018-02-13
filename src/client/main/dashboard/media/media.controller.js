0574a26db623e0c15add4a63843fea6cfbb5b5cf
meghana <megk0890@gmail.com>
2018-02-13 16:54:55 -0500

    Made changes in media for displaying check mark

--- src/client/main/dashboard/media/media.controller.js
+++ src/client/main/dashboard/media/media.controller.js
@@ -4,20 +4,31 @@ var media = require('./server/modules/media.js');
 app.controller('MediaController', function ($scope, $rootScope, $location, $timeout, AudioService) {
   $scope.audio = document.getElementById("audioTrack");
   $scope.playIcon = "play_arrow";
-
-  $scope.back = function () {
+  $scope.songs=[];
+    $scope.back = function () {
     $location.path('dashboard');
   };
 
-  $scope.getAudioState = function (initial) {
+  $scope.getAudioState = function (initial,index) {
     AudioService.getAudioState().then(function () {
       $timeout(function () {
         $scope.duration = AudioService.duration;
         $scope.playList = AudioService.playList;
+        $scope.songs=[];
+        for(var i=0;i<$scope.playList.length;i++){
+          $scope.songs.push({song: $scope.playList[i],selected:false});
+        }
         $scope.audioDir = AudioService.audioDir;
         $scope.playing = AudioService.playing;
-        console.log("PLAYING = "+$scope.playing);
-        if(!initial) $scope.currentIndex = AudioService.currentIndex;
+        if(!initial)
+          $scope.currentIndex = AudioService.currentIndex;
+        for(var i=0;i<$scope.songs.length;i++){
+            if(index === i){
+                $scope.songs[i].selected=true;
+            }else{
+                $scope.songs[i].selected= false;
+            }
+        }
         if ($scope.playing) {
           $scope.playIcon = "pause";
       }else{
@@ -51,7 +62,7 @@ app.controller('MediaController', function ($scope, $rootScope, $location, $time
 
   $scope.playSong = function (idx) {
     AudioService.playSong(idx).then(function () {
-      $scope.getAudioState();
+      $scope.getAudioState(false,idx);
     });
   };
 
