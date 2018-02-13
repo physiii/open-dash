bf64f1cb830b147ed1500d44fe13eb8d4a3d3d2d
Naga Sadam <nagasadam7@gmail.com>
2018-02-13 12:53:48 -0500

    move audio functions into service class

---
+++ src/client/main/dashboard/media/audio.service.js
@@ -0,0 +1,162 @@
+var app = angular.module('app');
+var media = require('./server/modules/media.js');
+
+app.service('AudioService', function () {
+  this.audioFiles = null;
+  this.playing = false;
+  this.playList = null;
+  this.audioDir = null;
+  this.currentIndex = 0;
+  this.currentTime = 0;
+  this.duration = -1;
+
+  this.audio = document.getElementById("audioTrack");
+
+  this.playMedia = function () {
+    var self = this;
+    return new Promise(function (resolve, reject) {
+      if (!self.audioFiles) {
+        var dir = "../../../media/music";
+        media.getAudioFiles(dir).then(function (files) {
+          self.playList = files;
+          self.audioDir = dir;
+          var audioFiles = files.map(function (file) {
+            return path.join(dir, file);
+          });
+          self.audioFiles = audioFiles;
+          self.playPauseAudio();
+          resolve(true);
+        }).catch(function(err) {
+          reject(err);
+        });
+      } else {
+        self.playPauseAudio();
+        resolve(true);
+      }
+    });
+  };
+
+  this.playPauseAudio = function () {
+    var self = this;
+    if (!this.audio) {
+      if (!audio.src) {
+        audio.src = this.audioFiles[0];
+        this.currentIndex = 0;
+      }
+      this.audio.load();
+      this.audio.addEventListener("loadedmetadata", function (metadata) {
+        self.duration = audio.duration;
+      });
+      this.audio.addEventListener("timeupdate", function (metadata) {
+        self.currentTime = audio.currentTime;
+      });
+    }
+    if (this.audio.paused) {
+      this.audio.play();
+      this.playing = true;
+    } else {
+      this.audio.pause();
+      this.playing = false;
+    }
+  };
+
+  this.skipPrevious = function () {
+    var self = this;
+    return new Promise(function (resolve, reject) {
+      if (!self.audioFiles)
+        return resolve(false);
+
+      if (self.currentIndex > 0) {
+        var audioPaused = self.audio.paused;
+        self.currentIndex -= 1;
+        self.audio.src = self.audioFiles[self.currentIndex];
+        self.audio.load();
+
+        if (!audioPaused)
+          self.audio.play();
+
+      }
+    });
+  };
+
+  this.skipNext = function () {
+    var self = this;
+    return new Promise(function (resolve, reject) {
+      if (!self.audioFiles)
+        return resolve(false);
+
+      if (self.currentIndex < self.audioFiles.length - 1) {
+        var audioPaused = self.audio.paused;
+        self.currentIndex += 1;
+        self.audio.src = self.audioFiles[self.currentIndex];
+        self.audio.load();
+
+        if (!audioPaused)
+          self.audio.play();
+
+      }
+    });
+  };
+
+  this.playSong = function (idx) {
+    var self = this;
+    return new Promise(function (resolve, reject) {
+      if (!self.audioFiles)
+        return resolve(false);
+      if (idx >= 0 && idx < self.audioFiles.length) {
+        var audioPaused = self.audio.paused;
+        self.audio.src = self.audioFiles[idx];
+        self.audio.load();
+        self.currentIndex = idx;
+        if (!audioPaused) {
+          self.audio.play();
+          self.playing = true;
+        } else {
+          self.playing = false;
+        }
+        return resolve(true);
+      }
+      resolve(false);
+    });
+  };
+
+  this.changeAudioDir = function (dir) {
+    var self = this;
+    return new Promise(function (resolve, reject) {
+      if (dir) {
+        dir = path.join("../../..", dir);
+      } else {
+        dir = "../../../media/music";
+      }
+      var dirChanged = self.audioDir != dir;
+      if (dirChanged || !self.audioFiles || !self.audioFiles.length) {
+        media.getAudioFiles(dir).then(function (files) {
+          self.playList = files;
+          self.audioDir = dir;
+          var audioFiles = files.map(function (file) {
+            return path.join(dir, file);
+          });
+          self.audioFiles = audioFiles;
+          resolve(true);
+        }).catch(function (err) {
+          console.log(err);
+          reject(err);
+        });
+      } else {
+        self.playList = files;
+        resolve(true);
+      }
+    });
+  };
+  this.getAudioState = function () {
+    var self = this;
+    return new Promise(function (resolve, reject) {
+      if (!self.audioFiles) {
+        return resolve(self.changeAudioDir());
+      }
+      else {
+        resolve(true);
+      }
+    });
+  };
+});
--- src/client/main/dashboard/media/media.controller.js
+++ src/client/main/dashboard/media/media.controller.js
@@ -1,128 +1,61 @@
 var app = angular.module('app');
 var media = require('./server/modules/media.js');
 
-app.controller('MediaController', function ($scope, $rootScope, $location, $timeout) {
-  $scope.playIcon = "play_arrow";
+app.controller('MediaController', function ($scope, $rootScope, $location, $timeout, AudioService) {
   $scope.audio = document.getElementById("audioTrack");
+  $scope.playIcon = "play_arrow";
+
+  $scope.back = function () {
+    $location.path('dashboard');
+  };
+
+  $scope.getAudioState = function (initial) {
+    AudioService.getAudioState().then(function () {
+      $timeout(function () {
+        $scope.duration = AudioService.duration;
+        $scope.playList = AudioService.playList;
+        $scope.audioDir = AudioService.audioDir;
+        $scope.playing = AudioService.playing;
+        console.log("PLAYING = "+$scope.playing);
+        if(!initial) $scope.currentIndex = AudioService.currentIndex;
+        if ($scope.playing) {
+          $scope.playIcon = "pause";
+      }else{
+          $scope.playIcon = "play_arrow";
+        }
+      }, 100);
+    });
+  }
 
   $scope.back = function () {
     $location.path('dashboard');
   };
 
   $scope.playMedia = function () {
-      if (!$scope.audioFiles) {
-          var dir = "../../../media/music";
-          media.getAudioFiles(dir).then(function (files) {
-              $scope.playList = files;
-              $scope.audioDir = dir;
-              var audioFiles = files.map(function (file) {
-                  return path.join(dir, file);
-              });
-              $scope.audioFiles = audioFiles;
-              $scope.playPauseAudio();
-          });
-      } else {
-          $scope.playPauseAudio();
-      }
-      if ($scope.audio && !$scope.audio.paused) {
-          $scope.playIcon = "pause";
-      }else{
-          $scope.playIcon = "play_arrow";
-      }
-  };
-
-  $scope.playPauseAudio =function() {
-    if (!$scope.audio) {
-        if (!audio.src) {
-            audio.src = $scope.audioFiles[0];
-            $scope.currentIndex = 0;
-        }
-        $scope.audio.load();
-        $scope.audio .addEventListener("loadedmetadata", function (metadata) {
-            $scope.duration = audio.duration;
-        });
-        $scope.audio .addEventListener("timeupdate", function (metadata) {
-            $scope.currentTime = audio.currentTime;
-        });
-    }
-    if ($scope.audio.paused) {
-        $scope.audio.play();
-    } else {
-        $scope.audio.pause();
-    }
+    AudioService.playMedia().then(function () {
+      $scope.getAudioState();
+    });
   };
 
   $scope.skipPrevious = function () {
-      if (!$scope.audioFiles)
-          return;
-
-      if ($scope.currentIndex > 0) {
-          var audioPaused = $scope.audio.paused;
-          $scope.currentIndex -= 1;
-          $scope.audio.src = $scope.audioFiles[$scope.currentIndex];
-          $scope.audio.load();
-
-          if (!audioPaused)
-              $scope.audio.play();
-
-          $scope.audioChange($scope.currentIndex);
-      }
+    AudioService.skipPrevious().then(function () {
+      $scope.getAudioState();
+    });
   };
 
   $scope.skipNext = function () {
-      if (!$scope.audioFiles)
-          return;
-      if ($scope.currentIndex < $scope.audioFiles.length - 1) {
-          var audioPaused = $scope.audio.paused;
-          $scope.currentIndex += 1;
-          $scope.audio.src = $scope.audioFiles[$scope.currentIndex];
-          $scope.audio.load();
-
-          if (!audioPaused)
-              $scope.audio.play();
-
-          $scope.audioChange($scope.currentIndex);
-      }
+    AudioService.skipNext().then(function () {
+      $scope.getAudioState();
+    });
   };
 
   $scope.playSong = function (idx) {
-      if (!$scope.audioFiles)
-          return;
-      if (idx >= 0 && idx < $scope.audioFiles.length ) {
-          var audioPaused = $scope.audio.paused;
-          $scope.audio.src = $scope.audioFiles[idx];
-          $scope.audio.load();
-          $scope.currentIndex = idx;
-          if (!audioPaused)
-              $scope.audio.play();
-
-         $scope.audioChange(idx);
-      }
+    AudioService.playSong(idx).then(function () {
+      $scope.getAudioState();
+    });
   };
 
-  $scope.audioChange = function(idx){
-
-  };
-
-  $scope.onload = function(){
-      var dir="";
-      if (dir) {
-          dir = path.join("../../..", dir);
-      } else {
-          dir = "../../../media/music";
-      }
-      var dirChanged = $scope.audioDir != dir;
-      if (dirChanged || !$scope.audioFiles || !$scope.audioFiles.length) {
-          media.getAudioFiles(dir).then(function (files) {
-              $scope.playList = files;
-              $scope.audioDir = dir;
-              var audioFiles = files.map(function (file) {
-                  return path.join(dir, file);
-              });
-              $scope.audioFiles = audioFiles;
-          });
-      } else {
-          $scope.playList=files;
-      }
+  $scope.onload = function () {
+    $scope.getAudioState(true);
   };
 });
