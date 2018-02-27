d065b27ca216ec652b1ae8503a81bee5588817ed
Naga sadam <nagasadam7@gmail.com>
2018-02-27 13:23:45 -0600

    start video streaming when camera page is loaded

--- src/client/main/dashboard/camera/camera.controller.js
+++ src/client/main/dashboard/camera/camera.controller.js
@@ -1,3 +1,14 @@
 var app = angular.module('app');
-app.controller('CanController', function(){
+var camera = require('./server/devices/camera.js');
+
+app.controller('CameraController', function ($scope, $rootScope, $location, $timeout) {
+
+  var canvas = document.getElementById('video-canvas');
+  camera.start_ffmpeg();
+  var url = 'ws://127.0.0.1:8082/';
+  var player = new JSMpeg.Player(url, {canvas: canvas});
+  $scope.$on('$destroy', function () {
+    console.log("Exit Camera Page");
+    camera.stop_ffmpeg();
+  });
 });
