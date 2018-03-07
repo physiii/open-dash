320b0ee708e6a728cbafc0aab5b47ed13962704b
Montana Rowe <calcnerd256@gmail.com>
2018-03-07 16:08:17 -0600

    order matters with pm2

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,4 @@
+sudo echo 'open	ALL=(ALL:ALL) NOPASSWD:ALL' >> /etc/sudoers
 sudo apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt update
@@ -35,6 +36,6 @@ cd ~
 git clone https://github.com/physiii/open-dash
 cd open-dash
 npm install
-sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u open --hp /home/open
+
 pm2 --name open-dash start ~/open-dash/scripts/dash-show.sh
-sudo echo 'open	ALL=(ALL:ALL) NOPASSWD:ALL' >> /etc/sudoers
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u open --hp /home/open
