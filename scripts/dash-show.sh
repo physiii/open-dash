db77bfcbbd1b9144acf604e7df0c7581cf99c33a
Montana Rowe <calcnerd256@gmail.com>
2018-03-13 16:02:19 -0500

    become an access point

--- scripts/dash-show.sh
+++ scripts/dash-show.sh
@@ -4,7 +4,7 @@ sudo ls || exit 1
 sudo rm /var/crash/*
 export DISPLAY=:0
 ~open/open-dash/scripts/display.sh
-~open/open-dash/scripts/disable-screensaver.sh
+~open/open-dash/scripts/fight-screensaver.sh &
 sudo eGTouchD
 
 bluetoothctl <<-EOF
@@ -14,6 +14,8 @@ bluetoothctl <<-EOF
 	trust 00:17: 	 
 	quit
 EOF
+
+sudo create_ap --daemon wlp3s0 enp2s0 dash1 
 
 cd ~open/open-dash/
 nw .
