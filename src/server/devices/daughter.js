75dbcffe633558ce9f4388e50fe0004157fb2711
Jeremy Pollock <jeremy-pollock@raider.rose.edu>
2018-05-29 16:16:22 -0500

    Added Daughter.js, created singleton class of DaughterCard, built basic Event emitter and serial port reciever for Daughter Card

---
+++ src/server/devices/daughter.js
@@ -0,0 +1,24 @@
+// ------------------------------  OPEN-DASH ----------------------------------- //
+// -----------------  https://github.com/physiii/Open-Dash -------------------- //
+// ---------------------------------- DAUGHTER.js ------------------------------------ //
+
+const EventEmitter = require('events'),
+  SerialPort = require('serialport'),
+  TAG = '[Daughter.js]';
+
+class DaughterCard {
+  constructor (data) {
+
+    this.serial_port = new SerialPort(data.SerialPort)
+    this.events = new EventEmitter();
+
+    this.serial_port.on(data){
+      const data = JSON.parse(data);
+      this.events.emit('CAN', {type: data.type, payload:data.payload});
+    }
+
+  }
+
+  }
+}
+module.eports = new DaughterCard();
