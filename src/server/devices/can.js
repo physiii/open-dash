c88a2132cae266d67c81247af89119b904449dda
Ethan Sisson <ethan@ethansisson.com>
2018-05-31 18:44:48 -0500

    Updated CAN module to support translating actions that have a start and end into events (e.g. pressing and holding a volume up button)

--- src/server/devices/can.js
+++ src/server/devices/can.js
@@ -1,6 +1,5 @@
 const EventEmitter = require('events'),
 	daughter = require('./daughter.js'),
-	canCache = {},
 	canEvents = {
 		'shift/unknown': {
 			event: 'shift'
@@ -8,6 +7,12 @@ const EventEmitter = require('events'),
 		'shift/reverse': {
 			event: 'shift',
 			shiftMode: 'reverse'
+		},
+		'steering-wheel/controls/volume-up': {
+			event: 'volume-up'
+		},
+		'steering-wheel/controls/volume-down': {
+			event: 'volume-down'
 		}
 	},
 	// TODO: These definitions are vehicle-specific. Move this into database?
@@ -25,8 +30,30 @@ const EventEmitter = require('events'),
 			canMessageIndex: 0,
 			canMessageValue: 'C0',
 			shouldValueMatch: false
+		},
+		{
+			name: 'steering-wheel/controls/volume-up',
+			canId: '6284000',
+			canMessageIndex: 0,
+			canMessageValue: '80',
+			shouldValueMatch: true,
+			end: {
+				canMessageValue: '00'
+			}
+		},
+		{
+			name: 'steering-wheel/controls/volume-down',
+			canId: '6284000',
+			canMessageIndex: 0,
+			canMessageValue: '40',
+			shouldValueMatch: true,
+			end: {
+				canMessageValue: '00'
+			}
 		}
-	];
+	],
+	canCache = {},
+	inProgressList = {};
 
 // TODO: Populate cache at initialization.
 
@@ -42,46 +69,92 @@ class Can {
 	}
 
 	_listenForCanMessages () {
-		daughter.on('CAN', (message) => {
-			this._getEventsForMessage(message).forEach((event) => {
-				this._events.emit(event.event, event);
+		daughter.on('CAN', (canMessage) => {
+			this._getEventsForMessage(canMessage).forEach((canEvent) => {
+				this._events.emit(canEvent.event, canEvent);
 			});
 		});
 	}
 
-	_getEventsForMessage (message) {
-		const changedCanValues = canDefinitions.filter((definition) => {
-			const canEvent = canEvents[definition.name],
-				messageId = message.id.toLowerCase(),
-				definitionId = definition.canId.toLowerCase(),
-				messageValue = message.message.substr(definition.canMessageIndex, definition.canMessageValue.length).toLowerCase(),
-				definitionValue = definition.canMessageValue.toLowerCase();
+	_getEventsForMessage (canMessage) {
+		return canDefinitions.map((canDefinition) => {
+			const canEvent = canEvents[canDefinition.name],
+				messageId = canMessage.id.toLowerCase(),
+				definitionId = canDefinition.canId.toLowerCase(),
+				cacheKey = canEvent.event;
+
+			let eventToEmit;
 
 			// Skip this definition if the IDs don't match.
 			if (messageId !== definitionId) {
 				return false;
 			}
 
-			// Check to see if the value matches for this definition.
-			if ((definition.shouldValueMatch && messageValue === definitionValue) || 
-				(!definition.shouldValueMatch && messageValue !== definitionValue)) {
+			// Check to see if this is the end of an event already in progress.
+			if (inProgressList[canDefinition.name] && this._doesMessageMatchDefinitionEnd(canMessage, canDefinition)) {
+				// Add this end event to the list of events to emit.
+				eventToEmit = {
+					...canEvent,
+					event: canEvent.event + '-end',
+					eventEnd: true
+				};
+
+				// Delete this from the list of in-progress events.
+				delete inProgressList[canDefinition.name];
+			} else if (this._doesMessageMatchDefinition(canMessage, canDefinition)) { // Check to see if the message value matches this definition.
+				// Add this event to the list of events to emit.
+				eventToEmit = {
+					...canEvent
+				};
+
+				// If the definition specifies that this is an event in
+				// progress (i.e. we expect to see an end condition in a later
+				// message), indicate that this is the start of an in-progress
+				// event and add this to the list of in-progress events.
+				if (canDefinition.end) {
+					inProgressList[canDefinition.name] = true;
+					eventToEmit.eventStart = true;
+				}
+			}
+
+			if (eventToEmit) {
+				const eventToEmitJson = JSON.stringify(eventToEmit);
+
 				// Check the cache to make sure the value actually changed.
-				if (canCache[canEvent.event] === canEvent) {
+				if (canCache[cacheKey] === eventToEmitJson) {
 					return false;
 				}
 
-				// Cache the new value so we can see if it changes in future CAN messages.
-				canCache[canEvent.event] = canEvent;
+				// Cache the event so we can see if future CAN messages result in a change.
+				canCache[cacheKey] = eventToEmitJson;
 
-				// Add this definition to the list of changes.
-				return true;
+				return eventToEmit;
 			}
 
 			return false;
-		});
+		}).filter((item) => item); // Remove empty indexes (from definitions that didn't match).
+	}
 
-		// Return the list of CAN events that correspond to the changed values.
-		return changedCanValues.map((definition) => canEvents[definition.name]);
+	_doesMessageMatchDefinition (canMessage, canDefinition) {
+		const messageValue = canMessage.message.substr(canDefinition.canMessageIndex, canDefinition.canMessageValue.length).toLowerCase(),
+			definitionValue = canDefinition.canMessageValue.toLowerCase();
+
+		return (canDefinition.shouldValueMatch && messageValue === definitionValue) || (!canDefinition.shouldValueMatch && messageValue !== definitionValue);
+	}
+
+	_doesMessageMatchDefinitionEnd (canMessage, canDefinition) {
+		if (!canDefinition.end) {
+			return false;
+		}
+
+		const messageValue = canMessage.message.substr(
+				canDefinition.end.canMessageIndex || canDefinition.canMessageIndex, // If it's provided, use a different index for the end value.
+				canDefinition.canMessageValue.length
+			).toLowerCase(),
+			definitionValue = canDefinition.end.canMessageValue.toLowerCase(),
+			shouldValueMatch = canDefinition.end.shouldValueMatch || canDefinition.shouldValueMatch; // If it's provided, use different matching logic for the end value.
+
+		return (shouldValueMatch && messageValue === definitionValue) || (!shouldValueMatch && messageValue !== definitionValue);
 	}
 }
 
