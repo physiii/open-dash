e780a35f83da880faf05a4688719059aeffd238e
Ethan Sisson <ethan@ethansisson.com>
2018-06-11 19:55:13 -0500

    More footer controller cleanup

--- src/client/main/footer/footer.controller.js
+++ src/client/main/footer/footer.controller.js
@@ -1,20 +1,20 @@
 const speakers = require('./server/devices/speakers.js');
 
 angular.module('app').controller('FooterController', function ($scope, $location, $timeout) {
-  function setMutedState (is_muted) {
-    $scope.mute_icon = is_muted ? 'volume_up' : 'volume_off';
-  }
+	function setMutedState (is_muted) {
+		$scope.mute_icon = is_muted ? 'volume_up' : 'volume_off';
+	}
 
-  speakers.getMuted().then(setMutedState);
+	speakers.getMuted().then(setMutedState);
 
-  $scope.showRemote = function () {
-    $location.path('remote');
-  }
+	$scope.showRemote = function () {
+		$location.path('remote');
+	}
 
-  $scope.mute = () => {
-    speakers.toggleMuted().then(setMutedState);
-  }
+	$scope.mute = () => {
+		speakers.toggleMuted().then(setMutedState);
+	}
 
-  $scope.raiseVolume = speakers.raiseVolume;
-  $scope.lowerVolume = speakers.lowerVolume;
+	$scope.raiseVolume = speakers.raiseVolume;
+	$scope.lowerVolume = speakers.lowerVolume;
 });
