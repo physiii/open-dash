d54ac09eb5f10cdfe5d86f8c74e1afb404fd1147
pi3 user <noreply@example.com>
2018-06-19 23:30:13 -0500

    refactor this a bit in preparation for passing to simple-agent

--- scripts/prepare_bluetooth_audio.sh
+++ scripts/prepare_bluetooth_audio.sh
@@ -5,8 +5,11 @@
 # on hte dash we must accept the pair request with pin 0000 and authorize the MDD's A2DP request
 #
 
+LOCALHCIDEV=hci0
 MDDBLUETOOTHADDR=${1}
-MYBLUETOOTHADDR=`hcitool dev | grep hci0 | awk '{print $2}'`
+#MDDBLUETOOTHADDR='B0:B4:48:A5:71:6E'
+
+MYBLUETOOTHADDR=`hcitool dev | grep ${LOCALHCIDEV} | awk '{print $2}'`
 
 echo -n $MYBLUETOOTHADDR
 
